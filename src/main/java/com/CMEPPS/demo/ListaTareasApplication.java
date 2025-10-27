bf3d4b45a776126046bd21e36bc844386af8a43e
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2025-10-27 18:21:22 +0100

    Proyecto base

---
+++ src/main/java/com/CMEPPS/demo/ListaTareasApplication.java
@@ -0,0 +1,13 @@
+package com.CMEPPS.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaTareasApplication.class, args);
+	}
+
+}
